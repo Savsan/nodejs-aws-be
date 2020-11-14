717bc6b1e2b3e47fa9629eff7cb057284dfb8d80
Alex <aliaksandr_varabyou@epam.com>
2020-11-14 12:11:18 +0300

    TASK-5: implemented SUB-TASK: 5.2

--- import-service/handlers/helpers.ts
+++ import-service/handlers/helpers.ts
@@ -1,4 +1,19 @@
+import csv from 'csv-parser';
+
 export const logErrorRelatedData = ({ event, error }): void => {
     console.log('ERROR: ', error);
     console.log('EVENT: ', event);
+};
+
+export const readObjectFromStream = (s3Stream): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        const result = [];
+        s3Stream
+            .pipe(csv())
+            .on('data', (data) => {
+                result.push(data);
+            })
+            .on('error', (error) => reject(error))
+            .on('end', () => resolve(result));
+    });
 };
--- import-service/handlers/importFileParser.ts
+++ import-service/handlers/importFileParser.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import AWS from 'aws-sdk';
 
-import { logErrorRelatedData } from './helpers';
+import { logErrorRelatedData, readObjectFromStream } from './helpers';
 
 import { DEFAULT_HEADERS, UPLOAD_FOLDER_NAME, PARSED_FOLDER_NAME } from './constants';
 
@@ -10,29 +10,52 @@ export const importFileParser: APIGatewayProxyHandler = async (event) => {
     console.log('EVENT_LOG: ', event);
     try {
         const { Records } = event;
+        const { REGION, BUCKET_NAME } = process.env;
         const s3 = new AWS.S3({
-            region: process.env.REGION,
+            region: REGION,
             signatureVersion: 'v4',
         });
 
         for (const record of Records) {
             const { object } = record.s3;
+            const data = await readObjectFromStream(
+                s3
+                    .getObject({
+                        Bucket: BUCKET_NAME,
+                        Key: object.key,
+                    })
+                    .createReadStream(),
+            );
+            console.log('DATA: ', data);
 
             await s3
                 .copyObject({
-                    Bucket: process.env.BUCKET_NAME,
-                    CopySource: object.key,
+                    Bucket: BUCKET_NAME,
+                    CopySource: `${BUCKET_NAME}/${object.key}`,
                     Key: object.key.replace(UPLOAD_FOLDER_NAME, PARSED_FOLDER_NAME),
                 })
                 .promise();
+
+            await s3
+                .deleteObject({
+                    Bucket: BUCKET_NAME,
+                    Key: object.key,
+                })
+                .promise();
         }
+
+        return {
+            statusCode: 202,
+        };
     } catch (error) {
         logErrorRelatedData({ event, error });
 
         return {
             statusCode: 500,
             headers: DEFAULT_HEADERS,
-            body: {},
+            body: {
+                message: 'Internal server error.',
+            },
         };
     }
 };
