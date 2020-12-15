9fce71e96f46a67aabf53f7457ca430e92b67838
Alex <aliaksandr_varabyou@epam.com>
2020-12-15 13:10:56 +0300

    TASK-9: fixes

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 require('dotenv').config();
-
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors({
+    // @ts-ignore
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
+  await app.listen(port);
 }
 
-bootstrap();
+bootstrap().then(() => {
+  console.log(`APP started on port ${port}`);
+});
